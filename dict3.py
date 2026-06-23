210fd0b5858357ca89c5e1891f614f8e1d731bb3
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-23 14:30:00 +0300

    pert 3 added

---
+++ dict3.py
@@ -0,0 +1,9 @@
+#sec 1
+me={"name":"moishi","addr":"jlm","children":"yes"}
+me.update({"fav_enimels":"dog","fav_food":"meet","religes":"is"})
+print(me)
+# sec 2
+list=[('a', 1), ('b', 2)]
+dict=dict([('a', 1), ('b', 2)])
+print(dict)
+
