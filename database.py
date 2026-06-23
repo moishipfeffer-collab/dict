41688b73f9f78e2e52c665e3141e8dbd03800f57
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-23 12:26:59 +0300

    part 1 dict

--- database.py
+++ database.py
@@ -15,6 +15,18 @@ print(agent)
 #sec 6
 del agent['active']
 print(agent)
+#sec 7
 print(agent.keys())
 print(agent.values())
 print(agent.items())
+#sec 8
+if "score" in agent:
+    print("yes")
+#sec 9
+scores = {'Alpha': 80, 'Bravo': 95, 'Charlie': 70}
+print(max(scores.values()))
+#sec 10
+agent2=agent.copy()
+agent2["level"]=4
+print(agent2,agent)
+
